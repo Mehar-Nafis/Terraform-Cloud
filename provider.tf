d60c3d8d488f2357be930bab2737352416f5c202
Mehar Nafis <108710069+Mehar-Nafis@users.noreply.github.com>
2024-12-02 13:55:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,11 @@
-provider "azurerm" {
+ provider "azurerm" {
   features {}
 
-  subscription_id = "b70f2b66-b08e-4775-8273-89d81847a0c2"
-  client_id       = "fa867aaa-bb30-4782-96a5-048f63ad030c"
-  client_secret   = "cuZ8Q~ps~h482tziPUstUtsUSCxalaV5~oW8sb-7"
-  tenant_id       = "52be5dcb-0af6-46a5-8bb0-7bda532bd7f0"
+  skip_provider_registration = "true"
+
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
