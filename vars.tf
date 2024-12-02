b448df971e498cf9c0e46713094cad0f3b8af3d4
Mehar Nafis <108710069+Mehar-Nafis@users.noreply.github.com>
2024-12-02 11:56:45 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,6 @@
 variable "client_id" {
   type = string
+  default = "fa867aaa-bb30-4782-96a5-048f63ad030c"
 }
 
 variable "client_secret" {
@@ -8,6 +9,7 @@ variable "client_secret" {
 
 variable "subscription_id" {
   type = string
+  default = "b70f2b66-b08e-4775-8273-89d81847a0c2"
 }
 
 variable "tenant_id" {
