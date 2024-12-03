c664d77ca124a84d4bad03643ec3183e2e104651
Mehar Nafis <108710069+Mehar-Nafis@users.noreply.github.com>
2024-12-03 11:15:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,10 +2,4 @@
   features {}
 
   skip_provider_registration = "true"
-
-  # Connection to Azure
-  ARM_SUBSCRIPTION_ID = var.subscription_id
-  ARM_CLIENT_ID = var.client_id
-  ARM_CLIENT_SECRET = var.client_secret
-  ARM_TENANT_ID = var.tenant_id
 }
